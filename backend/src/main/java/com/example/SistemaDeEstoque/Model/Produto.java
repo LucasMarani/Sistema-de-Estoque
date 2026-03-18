ccea34b91f908772c91ce692f7bdbf92ed98a4ca
Juliana Furlan <julianafurlancosta@edu.unifil.br>
2026-03-18 12:16:22 -0300

    Adicionando getter e setter no Produto.java

--- backend/src/main/java/com/example/SistemaDeEstoque/Model/Produto.java
+++ backend/src/main/java/com/example/SistemaDeEstoque/Model/Produto.java
@@ -35,4 +35,40 @@ public class Produto {
     }
 
     // getters e setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
 }
+
